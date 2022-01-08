eba9d1fec7f9d25efead4391f094219ff8255a5c
sanjaydraws <sprajapat8331@gmail.com>
2022-01-08 23:07:34 +0530

    initial commit

--- app/src/main/java/com/sanjayprajapat/in_app_purchase/MainActivity.kt
+++ app/src/main/java/com/sanjayprajapat/in_app_purchase/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sanjayprajapat.in_app_purchase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanjayprajapat.in_app_purchase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding?.apply {
+            setContentView(this.root)
+            executePendingBindings()
+            lifecycleOwner = this@MainActivity
+        }
+
     }
 }
